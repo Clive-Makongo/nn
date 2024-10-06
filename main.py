4a3ba5d053db7e18b6135964155aff7bad93b223
Clive-Makongo <clivemakongo@gmail.com>
2024-10-06 12:13:41 +0100

    lay out skeleton

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+## Start with input
+class NeuralNetwork:
+    def __init__(self, layer_sizes):
+        self.layer_sizes = layer_sizes
+        self.weights = []
+        self.biases = []
+
+        #initiali
+
+
+
+## Need code for a node
+
+## Weights
+
+## Am I Biased?
+
+## Output
+
+## GET BACK
+
